6a254051f099f063be442b5a9b0434d508d5d093
Eliud Berlin <info@eliud.io>
2021-04-15 18:40:32 +0100

    NullSafety running 1

--- lib/notifications_package.dart
+++ lib/notifications_package.dart
@@ -32,7 +32,7 @@ abstract class NotificationsPackage extends PackageWithSubscription {
   void _setState(bool newState, {MemberModel? currentMember}) {
     if (newState != state_CONDITION_MEMBER_HAS_UNREAD_NOTIFICATIONS) {
       state_CONDITION_MEMBER_HAS_UNREAD_NOTIFICATIONS = newState;
-      accessBloc.add(MemberUpdated(currentMember));
+      accessBloc!.add(MemberUpdated(currentMember));
     }
   }
 
